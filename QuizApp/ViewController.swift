6ed32b758340f6170068ef29c6842c59b5021bcb
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-12 18:00:16 +0900

    first comit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by 中村泰輔 on 2019/08/12.
+//  Copyright © 2019 icannot.t.n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
