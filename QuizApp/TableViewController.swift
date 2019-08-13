ab1aab21503a879579a79557a449f5e3784262e4
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-13 13:40:40 +0900

    ボタンを隠す

--- QuizApp/TableViewController.swift
+++ QuizApp/TableViewController.swift
@@ -9,15 +9,13 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
 
     // MARK: - Table view data source
@@ -29,18 +27,18 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 3
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
-
+        
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -11,21 +11,25 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var questionTitle: UINavigationItem!
-     @IBOutlet weak var questionText: UITextView!
+    @IBOutlet weak var questionText: UITextView!
+    @IBOutlet weak var stackViewButton: UIStackView!
     
     
+    // 問題データ
     let questions : [String] = [
         "日本の世界遺産『富士山－信仰の対象と芸術の源泉』は、2013年に（ ）として世界遺産登録されました。\n 1. 文化遺産 2. 自然遺産 3. 山岳遺産 4. 伝統遺産",
         "イタリア共和国の世界遺産『フィレンツェの歴史地区』のあるフィレンツェを中心に、17世紀に栄えた芸術運動は何でしょうか。\n 1. シュルレアリスム2. アバンギャルド 3. ルネサンス",
         "2016年のオリンピック開催地であるリオ・デ・ジャネイロで、ブラジル独立100周年を記念して作られたキリスト像が立つ場所として、正しいものはどれか。 \n 1. コパカバーナの山 2. コルコバードの丘"
          ]
     
-    
+    // 正解番号
     let answersNum : [Int] = [1,3,2]
-
-    var questionCount :Int = 0
-    var buttonTag : Int = 1
-    
+    // 問題番号
+    var questionNumber :Int = 1
+    // ボタン番号総数
+    var buttonTagSum : Int = 4
+    // 問題カウント
+    var questionsCount : Int = 0
    
     
    
@@ -33,17 +37,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       
+        
         showQuestion()
         
     }
     
    
-    
+    // 問題を表示
     func showQuestion() {
-        
-       questionText.text = questions[questionCount]
-        
-       questionTitle.title = "第\(questionCount + 1)問"
+        // テキストに問題を代入
+       questionText.text = questions[questionsCount]
+        // タイトルに何問目か表示
+       questionTitle.title = "第\(questionsCount + 1)問"
         
     }
     
@@ -51,12 +57,15 @@ class ViewController: UIViewController {
         
         
         
-        if  selectedNum == answersNum[questionCount] {
+        if  selectedNum == answersNum[questionsCount] {
             
-            questionCount += 1
+            
             showCorrectAlert(title: "正解", message: "次へ進みます")
-            
+            questionsCount += 1
+            hideButton()
             showQuestion()
+            
+          
             
         } else {
             
@@ -67,20 +76,26 @@ class ViewController: UIViewController {
         
     }
     
-    
-    
-    
-    
-    
-    func  nextQuestion() {
+    func hideButton() {
         
-        if questionCount < questions.count {
-            
-            questionCount += 1
-            
-        }
+        
+        let buttonCount :Int  =  stackViewButton.arrangedSubviews.count
+        
+        let hideButtonNumber : Int = buttonCount - questionNumber
+        
+        stackViewButton.arrangedSubviews[hideButtonNumber].isHidden = true
+       
+        questionNumber += 1
+    }
+    
+    
+    func reset() {
+        
+        
         
     }
+    
+   
     
     
     func showCorrectAlert(title : String? , message : String){
@@ -98,8 +113,11 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let goforward = UIAlertAction(title: "OK", style: .default, handler: {  (action: UIAlertAction!) in
             
-             self.questionCount += 1
+             self.hideButton()
+             self.questionsCount += 1
              self.showQuestion()
+            
+             
             
         })
         let cancel = UIAlertAction(title: "もういちど", style: .cancel, handler: {(action: UIAlertAction!) in
@@ -112,10 +130,20 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
         }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "showResult", let vc = segue.destination as? TableViewController else {
+            return
+        }
+        
+    }
+    
     
     @IBAction func tappedButton(_ sender: UIButton) {
         
+        
+        
          checkAnswer(sender.tag)
+        
        
         }
         
